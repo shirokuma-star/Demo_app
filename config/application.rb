f09cfc73923d2dd99ac57230f2cb439dfcc0a4db
shirokuma-star <takakenhowaken@gmail.com>
2021-03-28 14:43:50 +0900

    Feature: devise user and bootstrap

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module DemoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    # 以下を追加すれば日本語に
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更するなら，以下を追加
+    config.time_zone = 'Asia/Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
